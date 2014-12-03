66c7adf3ce0c4c978f0a2c87cdd39ea86d67ed17
Drew Dulgar <drewbdulgar@gmail.com>
2014-12-03 15:46:22 -0500

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,13 @@
 $(document).ready(function()
 {
+  $('.contact-section').on('mouseenter', '.contact-item', function() 
+  {
+    $('.icon .fa', $(this)).addClass('contact-item-icon-hover');
+  });
+  
+  $('.contact-section').on('mouseleave', '.contact-item', function() 
+  {
+    $('.icon .fa', $(this)).addClass('contact-item-icon-hover');
+  });
   
 });
